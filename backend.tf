d30091421eda77ba5809c7051247765422d62be3
Tirupatitata123 <tirupati.boyane@gmail.com>
2026-03-05 14:18:21 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform{
 backend "s3" {
   bucket = "tirupatiboyane"
-  key    = "project1/terraform.tfstate"
+  key    = "terraform.tfstate"
   region = "ap-south-1"
   dynamodb_table = "terraform-lock-table"
   encrypt        = true
