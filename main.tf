7e770538f174c419df5d3eaf84a011f4efc6b633
Tirupatitata123 <tirupati.boyane@gmail.com>
2026-03-05 15:21:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "jenkins_ec2" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-019715e0d74f695be"
   instance_type = "t2.micro"
 
   tags = {
