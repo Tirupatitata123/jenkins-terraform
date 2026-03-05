c4b3264d83361796c4954bd6bc476f2ef587bbbf
Tirupatitata123 <tirupati.boyane@gmail.com>
2026-03-05 15:15:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,11 +14,11 @@ provider "aws" {
   region  = "ap-south-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-0f5ee92e2d63afc18"
+resource "aws_instance" "jenkins_ec2" {
+  ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "jenkinsinfra1"
+    Name = "Jenkins-Terraform-EC2"
   }
 }
